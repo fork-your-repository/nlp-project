831ce9653c87a660615c276b10669aa3cae38854
Miatta Sinayoko <miatta.sinayoko@gmail.com>
2023-08-20 22:11:34 -0500

    update file

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy import stats
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
